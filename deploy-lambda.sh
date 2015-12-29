556d8fd5592dd282fd13a54097cb75dd50c5ccfa
Jakub Janczak <jakub.janczak@schibsted.pl>
2015-12-29 08:59:46 +0100

    Making the lambda file relative to DIR

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -9,7 +9,7 @@ echo "Retrieving properties"
 printenv | grep -v "^_.*" | sed -e 's/^\([^\=]*\)=\(.*\)$/\1=\"\2\"/' > "${PROPERTIES_FILE}"
 
 echo "Putting properties into jarfile"
-${BUILDPACK_DIR}/properties-weaver/weave.sh "${_LAMBDA_JAR_FILE}" "${PROPERTIES_FILE}" "${OUTPUT_FILE}" 
+${BUILDPACK_DIR}/properties-weaver/weave.sh "${DIR}/${_LAMBDA_JAR_FILE}" "${PROPERTIES_FILE}" "${OUTPUT_FILE}" 
 
 echo "Deploying lambda"
 AWS_ACCESS_KEY_ID="${_AWS_ACCESS_KEY_ID}" AWS_SECRET_ACCESS_KEY="${_AWS_SECRET_ACCESS_KEY}" AWS_DEFAULT_REGION="${_AWS_DEFAULT_REGION}" ${DIR}/.heroku/python/bin/aws update-function --function-name "${_LAMBDA_FUNCTION_NAME}" --zip-file "fileb://${OUTPUT_FILE}"
