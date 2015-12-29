645ffd1a7aab2a2b202e14481c9b90d46a2663de
Jakub Janczak <jakub.janczak@schibsted.pl>
2015-12-29 01:03:19 +0100

    Making the output file explicit

--- properties-weaver/weave.sh
+++ properties-weaver/weave.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then
-    echo "$0 <input_jar_file> <properties_file>"
+if [ $# -lt 3 ]; then
+    echo "$0 <input_jar_file> <properties_file> <output_jar_file>"
     exit 1
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INPUT_FILE="$1"
 PROPERTIES_FILE="$2"
-BASE_INPUT_FILE=$(basename "$INPUT_FILE")
-OUTPUT_FILE="${DIR}/target/${BASE_INPUT_FILE%.jar}-with-properties.jar"
+OUTPUT_FILE="$3"
 
 mvn -f "${DIR}/weave-pom.xml" antrun:run -DinputFile="${INPUT_FILE}" -DoutputFile="${OUTPUT_FILE}" -DpropertiesFile="${PROPERTIES_FILE}"
 
