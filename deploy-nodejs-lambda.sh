482cca26b9ae1f6ae89a6820874c66752b4342d5
Jakub Janczak <jakub.janczak@schibsted.pl>
2016-01-31 21:45:52 +0100

    Those vars are defined elsewhere

--- deploy-nodejs-lambda.sh
+++ deploy-nodejs-lambda.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-_LAMBDA_FUNCTION_ARN="${_LAMBDA_FUNCTION_ARN:?You need to set _LAMBDA_FUNCTION_ARN}"
-_AWS_SECRET_ACCESS_KEY="${_AWS_SECRET_ACCESS_KEY:?You need to set _AWS_SECRET_ACCESS_KEY}"
-_AWS_ACCESS_KEY_ID="${_AWS_ACCESS_KEY_ID:?You need to set _AWS_ACCESS_KEY_ID}"
-_AWS_DEFAULT_REGION="${_AWS_DEFAULT_REGION:?You need to set _AWS_DEFAULT_REGION}"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIR}/deploy-common.sh"
 
